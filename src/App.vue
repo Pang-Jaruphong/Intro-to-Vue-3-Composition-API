450ec2a3e3ed74a736f19be09317f5277fd2d2b2
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2025-11-03 15:29:50 +0100

    Fin Exerce 2

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,21 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+const product = 'Chaussettes bleus';
+const description = 'Les meilleures chaussettes de training jamais faites !';
+const image = '/src/assets/images/socks_blue.jpeg';
+const url ='http://localhost:5175/sockbleu';
+</script>
 
 <template>
   <div class="product-display">
     <div class="product-container">
+
       <div class="product-info">
-        <h1>Socks</h1>
+        <h1> {{ product }} </h1>
+
+        <img v-bind:src="image" alt = "Chausettes">
+        <h2> {{ description }} </h2>
+        <a :href="url" target="_blank">Voir ce produit</a>
       </div>
     </div>
   </div>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,14 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
+App.data = function () {
+
+    // la fonction pour définir l'information pour les produits
+    return {
+        url:"src/assets/images/socks_blue.jpeg"
+    }
+}
+
 import './assets/main.css'
 
 createApp(App).mount('#app')

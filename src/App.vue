27cc110ba07e8b7e397e7e37bd1e848110c12000
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2025-11-03 16:33:56 +0100

    Travailler jusqu'à c) v-show

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <script setup>
 import { ref } from 'vue'
 
-const product = ref('Socks')
-
+const product = ref('Socks');
+const image = '/src/assets/images/socks_blue.jpeg';
+const inStock =ref(false)
 </script>
 
 <template>
@@ -10,9 +11,14 @@ const product = ref('Socks')
     <div class="product-container">
       <div class="product-image">
         <!-- image goes here -->
+        <img v-bind:src="image" alt="Chausettes">
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <p v-show = "inStock">En stock</p>
+        <p v-show = "!inStock">Rupture de stock</p>
+        <p v-if = "inStock">En stock</p>
+        <p v-else> En rupture de stock</p>
       </div>
     </div>
   </div>

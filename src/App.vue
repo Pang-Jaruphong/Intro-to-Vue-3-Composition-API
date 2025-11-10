43e4359d9315d424abe4388423f2abc282c03973
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2025-11-10 10:29:40 +0100

    Fin exercices 4

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,14 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 
 const product = ref('Socks')
 const image = ref(socksGreenImage)
-
+const detail = { coton : '50%', laine : '30%', polyester : '20%'}
+// propriété variants contenant les ids et les couleurs
+const variants = [
+  {id:2234, color :'green'},
+  {id:2235, color : 'blue'}
+]
+const sizes = ['S', 'M', 'L', 'XL'];
+// v-for la directive pour la gestion des boucles et :key= pour la gestion des performances des listes
 </script>
   
 <template>
@@ -16,6 +23,14 @@ const image = ref(socksGreenImage)
       </div>
       <div class="product-info">
         <h1>{{ product }}</h1>
+        <ul>
+          <li v-for = "size in sizes" :key="size"> {{size}} </li>
+        </ul>
+        <ul>
+          <li> Coton : {{detail.coton}} </li>
+          <li> Laine : {{detail.laine}} </li>
+          <li> Polyester : {{detail.polyester}} </li>
+        </ul>
       </div>
     </div>
   </div>

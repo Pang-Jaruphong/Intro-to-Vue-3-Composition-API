cc11d7ec4b5d0a16478a13d52682a1ac9f4280c2
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2025-11-10 14:01:49 +0100

    Ajouter brand avec le nom du produit

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@ import socksGreenImage from './assets/images/socks_green.jpeg'
 import socksBlueImage from './assets/images/socks_blue.jpeg'
 
 const product = ref('Socks')
-const brand = ref('Vue Mastery')
+const brand = ref('CPNV')
 
 const image = ref(socksGreenImage)
 const inStock = ref(false)
@@ -33,7 +33,7 @@ const updateImage = (variantImage) => image.value = variantImage
         <img v-bind:src="image">
       </div>
       <div class="product-info">
-        <h1>{{ product }}</h1>
+        <h1> {{brand}} {{ product }}</h1>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
         <ul>

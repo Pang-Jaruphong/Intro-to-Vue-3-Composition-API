69b1020da8fc5b322a83f5abd34b3345f1d02792
Pang-Jaruphong <pv82wpf@eduvaud.ch>
2025-12-01 09:35:38 +0100

    On sale avec deux conditions

--- src/App.vue
+++ src/App.vue
@@ -10,31 +10,52 @@ const brand = ref('CPNV')
 // Définir la propriété calculée
 const title = computed(() =>{
   // Concaténation de la marque et du nom du produit
-  return brand.value + '' + product.value
+  return brand.value + ' ' + product.value
 })
 
-const image = ref(socksGreenImage)
-const inStock = ref(true)
+const image = computed(() => {
+  // Retourne l'URL de l'image de la variante sélectionée
+  return variants.value[selectedVariant.value].image
+    })
+const inStock = computed(() => {
+  // Ajouter la condition s'il y a dans le stock
+  return variants.value[selectedVariant.value].quantity > 0
+})
   
 const details = ref(['50% cotton', '30% wool', '20% polyester'])
 
 const variants = ref([
   { id: 2234, color: 'green', image: socksGreenImage, quantity : 10},
-  { id: 2235, color: 'blue', image: socksBlueImage, quantity :0},
+  { id: 2235, color: 'blue', image: socksBlueImage, quantity : 3},
 ])
 
 const cart = ref(0)
 
-const addToCart = () => cart.value += 1
+const addToCart = () => {
+  // Incrémenter dans le panier global
+  cart.value += 1
 
-const updateImage = (variantImage) => image.value = variantImage
-
-methods : {
-  updatevariant : fonction (index){
-    this.selectedVariant =index
-    this.variants[index].variantSoldOut = this.variants[index].quantity === 0;
-  }
+  // Décrémenter la quantité de la variante actuellement sélectionnée
+  variants.value[selectedVariant.value].quantity -= 1
 }
+
+const selectedVariant = ref(0)
+
+const updateVariant = (index) => {
+  selectedVariant.value = index
+      console.log ('Selected Variant index : '  + index)
+}
+
+const onSale = ref(true)
+
+// si les deux conditions sont True, onSale && inStock
+const sale = computed(() => {
+  if (onSale.value) {
+    return ' est en action'
+  }
+  return ' '
+})
+
 </script>
   
 <template>
@@ -46,25 +67,26 @@ methods : {
         <img v-bind:src="image">
       </div>
       <div class="product-info">
-        <h1> {{title}} </h1>
+        <h1> {{title}} {{sale}}</h1>
+        <p v-if="onSale && inStock"> En Solde</p>
         <p v-if="inStock">In Stock</p>
         <p v-else>Out of Stock</p>
-        <ul>
+       <ul>
           <li v-for="detail in details">{{ detail }}</li>
         </ul>
         <div 
           v-for="(variant, index) in variants"
-          :key="variant.variantid"
+          :key="variant.id"
           @mouseover="updateVariant(index)"
           class="color-circle"
           :style="{ backgroundColor: variant.color }"
         >
         </div>
         <button
-          class="button" 
-          :class="{ disabledButton: !inStock }"
+          class="button"
           :disabled="!inStock"
           v-on:click="addToCart"
+          :class="{ disabledButton: !inStock }"
         >
           Add to cart
         </button>
